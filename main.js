d84237301fe74372334faa043638d2ddc4cd8963
marcopriests <prtmrc98@gmail.com>
2025-07-17 19:22:26 +0200

    [DONE] Lezione completata

---
+++ main.js
@@ -0,0 +1,51 @@
+// import dayjs from 'dayjs';
+
+async function fetchJson(url) {
+    const response = await fetch(url);
+    const obj = await response.json();
+    return obj;
+};
+
+async function getChefBirthday(id) {
+    let ricetta;
+
+    try {
+        ricetta = await fetchJson(`https://dummyjson.com/recipes/${id}`);
+    } catch (error) {
+        throw new Error("Errore nel trovare la ricetta", error.message);
+    };
+
+    if (ricetta.message) {
+        throw new Error(`Ricetta con id ${id} non trovata`);
+    };
+
+    let user;
+
+    try {
+        user = await fetchJson(`https://dummyjson.com/users/${ricetta.userId}`);
+    } catch (error) {
+        throw new Error("Errore nel trovare l'user", error.message);
+    };
+
+    if (user.message) {
+        throw new Error(`User con id ${ricetta.userId} non trovato`);
+    };
+
+
+    return user.birthDate;
+};
+
+// getChefBirthday(1)
+//     .then(birthDate => console.log('Data di nascita dello chef:', birthDate))
+//     .catch(err => console.error(err));
+
+(async () => {
+    try {
+        const result = await getChefBirthday(1);
+        console.log('Data di nascita dello chef:', dayjs(result).format('DD/MM/YYYY'));
+    } catch (error) {
+        console.error(error);
+    } finally {
+        console.log('Fine del codice!');
+    };
+})();
